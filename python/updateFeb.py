93fa149bc167c5e559a8f38c701c4f2ec49b28ea
Larry Ruckman <ruckman@slac.stanford.edu>
2019-03-20 10:58:11 -0700

    adding standalone root class

--- python/updateFeb.py
+++ python/updateFeb.py
@@ -8,8 +8,11 @@
 # copied, modified, propagated, or distributed except according to the terms 
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
-
-import ClinkDev
+import pyrogue as pr
+import rogue.hardware.axi
+import rogue.protocols
+import XilinxKcu1500Pgp as kcu1500
+import ClinkFeb as feb
 import sys
 import argparse
 import time
@@ -58,7 +61,75 @@ args = parser.parse_args()
 
 #################################################################
 
-cl = ClinkDev.ClinkDev(
+class MyRoot(pr.Root):
+
+    def __init__(self,
+            name        = 'ClinkDev',
+            description = 'Container for CameraLink Dev',
+            dev         = '/dev/datadev_0',# path to PCIe device
+            version3    = False,           # true = PGPv3, false = PGP2b
+            pollEn      = True,            # Enable automatic polling registers
+            initRead    = True,            # Read all registers at start of the system
+            **kwargs):
+        super().__init__(name=name, description=description, **kwargs)
+        
+        # Create PCIE memory mapped interface
+        self.memMap = rogue.hardware.axi.AxiMemMap(dev)     
+
+        # Set the timeout
+        self._timeout = 1.0 # 1.0 default
+        
+        # PGP Hardware on PCIe 
+        self.add(kcu1500.Hardware(            
+            memBase  = self.memMap,
+            numLane  = 4,
+            version3 = version3,
+            # expand   = False,
+        ))   
+
+        # Create arrays to be filled
+        self._dma = [[None for vc in range(4)] for lane in range(4)] # self._dma[lane][vc]
+        self._srp =  [None for lane in range(4)]
+        
+        # Create the stream interface
+        for lane in range(4):
+        
+            # PCIe DMA interface
+            self._dma[lane][0] = rogue.hardware.axi.AxiStreamDma(dev,(0x100*lane)+0,True) # VC0 = Registers
+            self._dma[lane][1] = rogue.hardware.axi.AxiStreamDma(dev,(0x100*lane)+1,True) # VC1 = Data
+            self._dma[lane][2] = rogue.hardware.axi.AxiStreamDma(dev,(0x100*lane)+2,True) # VC2 = Serial
+            self._dma[lane][3] = rogue.hardware.axi.AxiStreamDma(dev,(0x100*lane)+3,True) # VC3 = Serial
+            
+            # Disabling zero copy on the data stream
+            self._dma[lane][1].setZeroCopyEn(False)    
+                
+            # SRP
+            self._srp[lane] = rogue.protocols.srp.SrpV3()
+            pr.streamConnectBiDir(self._dma[lane][0],self._srp[lane])
+                     
+            # CameraLink Feb Board
+            self.add(feb.ClinkFeb(      
+                name        = (f'ClinkFeb[{lane}]'), 
+                memBase     = self._srp[lane], 
+                serialA     = self._dma[lane][2],
+                serialB     = self._dma[lane][3],
+                camTypeA    = 'Opal000', # Assuming OPA 1000 camera
+                camTypeB    = 'Opal000', # Assuming OPA 1000 camera
+                version3    = version3,
+                enableDeps  = [self.Hardware.PgpMon[lane].RxRemLinkReady], # Only allow access if the PGP link is established
+                expand      = False,
+            ))         
+        
+        # Start the system
+        self.start(
+            pollEn   = pollEn,
+            initRead = initRead,
+            timeout  = self._timeout,
+        )
+
+#################################################################
+
+cl = MyRoot(
     dev      = args.dev,
     version3 = args.version3,
     pollEn   = False,
